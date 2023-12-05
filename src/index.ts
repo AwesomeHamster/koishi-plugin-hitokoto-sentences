6ce726c1b14ab9fddae6ddc5bc2ba73eb673b973
SaarChaffee <saarchaffee@qq.com>
2023-12-05 16:54:44 +0800

    feat: get sentences from local

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,60 @@
-import { Context, Logger, Schema } from 'koishi'
+import { Context, Service } from 'koishi'
 
-export const name = 'hitokoto-sentences'
+export const name = 'sentences'
 
-const logger = new Logger('hitokoto-sentences')
+declare module 'koishi' {
+  interface Context {
+    'sentences': Sentences
+  }
+}
 
-export interface Config { }
+export class Sentences extends Service {
+  private s: {
+    [key: string]: Sentences[]
+  } = {}
 
-export const Config: Schema<Config> = Schema.object({})
+  constructor(ctx: Context) {
+    super(ctx, 'sentences', true)
+    this.s = {
+      a: require('../data/a.json'),
+      b: require('../data/b.json'),
+      c: require('../data/c.json'),
+      d: require('../data/d.json'),
+      e: require('../data/e.json'),
+      f: require('../data/f.json'),
+      g: require('../data/g.json'),
+      h: require('../data/h.json'),
+      i: require('../data/i.json'),
+      j: require('../data/j.json'),
+      k: require('../data/k.json'),
+      l: require('../data/l.json'),
+    }
+  }
+
+  getAllByType(type: string): Sentences[] {
+    return this.s[type]
+  }
+
+  getRandomOneByType(type: string): Sentences {
+    return this.s[type][Math.floor(Math.random() * this.s[type].length)]
+  }
+}
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.plugin(Sentences)
+}
+
+export interface Sentences {
+  id: number
+  hitokoto: string
+  type: string
+  from: string
+  from_who: string | null
+  creator: string
+  creator_uid: number
+  reviewer: number
+  uuid: string
+  commit_from: string
+  created_at: string
+  length: number
 }
